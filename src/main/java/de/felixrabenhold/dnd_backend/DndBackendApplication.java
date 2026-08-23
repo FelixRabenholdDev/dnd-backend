be607977f5de686a37ef5cd854483158528fbb80
FelixRabenholdDev <226058561+FelixRabenholdDev@users.noreply.github.com>
2026-08-23 18:05:26 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/de/felixrabenhold/dnd_backend/DndBackendApplication.java
@@ -0,0 +1,13 @@
+package de.felixrabenhold.dnd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndBackendApplication.class, args);
+	}
+
+}
